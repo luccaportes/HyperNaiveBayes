c87adaeaff0049314b96e40077ba77473d7e87b4
Lucca Portes <lucca@Luccas-Mac-mini.local>
2020-02-03 11:32:23 -0300

    comments added

--- nb.py
+++ nb.py
@@ -9,12 +9,15 @@ class NB:
         self.stats_per_class = collec.defaultdict(dict)
 
     def fit(self, x, y):
+        # Split the instances per class
         classes = np.unique(y)
         for c in classes:
             indexes = y == c
             c_x = x[indexes]
             self.dict_per_class[c] = c_x
 
+        # Compute the probability of each individual feature happen in each class
+        # n_occurences / total_instances_class
         for label, feats in self.dict_per_class.items():
             n_inst = len(feats)
             for col in feats.columns:
@@ -22,16 +25,21 @@ class NB:
 
     def predict(self, x):
         existing_cols = []
+        # Check which features are different than zero
         for col in x.columns:
             if x[col].tolist()[0] == 1:
                 existing_cols.append(col)
         prob_cls = {}
+        # For each previously computed statistics for each class
         for cls, stats in self.stats_per_class.items():
             prob = 1
+            # multiply the probability of coexisting features
             for feat in existing_cols:
                 prob *= stats[feat][1]
+            # Compute the probability of the set of features belonging to each class
             n_instances = prob * len(self.dict_per_class[cls])
             prob_cls[cls] = n_instances / len(self.dict_per_class[cls])
+        # Select the biggest one and return
         max_prob = -1
         answer = None
         for k, v in prob_cls.items():
